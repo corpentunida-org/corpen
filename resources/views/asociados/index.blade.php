ff7589f38467570f37b1ea9cbff090e7cf02d026
Vanessa <nva.gomo926@gmail.com>
2024-05-23 16:25:40 -0500

    imagen logo

--- resources/views/asociados/index.blade.php
+++ resources/views/asociados/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
@@ -8,25 +9,18 @@
     <title>
         Asociados
     </title>
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
-        name='viewport' />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../assets/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link href="../assets/demo/demo.css" rel="stylesheet" />
-
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/pruebacss.css') }}"> --}}
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/now-ui-dashboard.css?v=1.5.0') }}" rel="stylesheet">
     <link href="{{ asset('assets/demo/demo.css') }}" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -47,8 +41,7 @@
                         </div>
                         <a class="navbar-brand" href="#">Asociados</a>
                     </div>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
-                        aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-bar navbar-kebab"></span>
                         <span class="navbar-toggler-bar navbar-kebab"></span>
                         <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -58,8 +51,7 @@
                         {{-- Buscador Cedulaaaa --}}
                         <form action="{{ route('asociados.show', ['asociado' => 'ID']) }}" method="GET">
                             <div class="input-group no-border">
-                                <input type="text" name="id" value="" class="form-control"
-                                    placeholder="Search...">
+                                <input type="text" name="id" value="" class="form-control" placeholder="Search...">
                                 <div class="input-group-append">
                                     <button class="input-group-text" type="submit">
                                         <i class="now-ui-icons ui-1_zoom-bold"></i>
@@ -68,7 +60,7 @@
                             </div>
                         </form>
 
-                        <ul class="navbar-nav">                            
+                        <ul class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link" href="/user/profile">
                                     <i class="now-ui-icons users_single-02"></i>
@@ -111,37 +103,36 @@
                                         </thead>
                                         <tbody>
                                             @foreach ($asociados as $asociado)
-                                                <tr>
-                                                    <td>{{ $asociado->cedula }}</td>
-                                                    <td>{{ $asociado->apellido . ' ' . $asociado->nombre }}</td>
+                                            <tr>
+                                                <td>{{ $asociado->cedula }}</td>
+                                                <td>{{ $asociado->apellido . ' ' . $asociado->nombre }}</td>
 
-                                                    @if ($asociado->distrito_id !== 0)
-                                                        <td>{{ $asociado->distrito->id }}</td>
-                                                    @else
-                                                        <td></td>
-                                                    @endif
+                                                @if ($asociado->distrito_id !== 0)
+                                                <td>{{ $asociado->distrito->id }}</td>
+                                                @else
+                                                <td></td>
+                                                @endif
 
-                                                    @if ($asociado->ciudad_id !== null && $asociado->ciudad_id !== 0)
-                                                        <td>{{$asociado->ciudade->nombre}}
-                                                        </td>
-                                                    @else
-                                                        <td></td>
-                                                    @endif
+                                                @if ($asociado->ciudad_id !== null && $asociado->ciudad_id !== 0)
+                                                <td>{{$asociado->ciudade->nombre}}
+                                                </td>
+                                                @else
+                                                <td></td>
+                                                @endif
 
-                                                    {{-- <td>{{ $asociado->direccion }}</td> --}}
+                                                {{-- <td>{{ $asociado->direccion }}</td> --}}
 
-                                                    @if ($asociado->estado)
-                                                        <td><button type="button"
-                                                                class="btn btn-success py-1">ACTIVO</button></td>
-                                                    @else
-                                                        <td></td>
-                                                    @endif
+                                                @if ($asociado->estado)
+                                                <td><button type="button" class="btn btn-success py-1">ACTIVO</button></td>
+                                                @else
+                                                <td></td>
+                                                @endif
 
-                                                    <td>{{ $asociado->celular }}</td>
-                                                    {{-- <td>{{ $asociado->email }}</td>
-                                                    <td>{{ $asociado->observacion }}</td>
-                                                    <td>{{ $asociado->observacion_familia }}</td> --}}
-                                                </tr>
+                                                <td>{{ $asociado->celular }}</td>
+                                                {{-- <td>{{ $asociado->email }}</td>
+                                                <td>{{ $asociado->observacion }}</td>
+                                                <td>{{ $asociado->observacion_familia }}</td> --}}
+                                            </tr>
                                             @endforeach
                                         </tbody>
                                     </table>
@@ -157,34 +148,122 @@
                                         </a>
                                     </li>
                                     @php
-                                        $start = max(1, $asociados->currentPage() - 1);
-                                        $end = min($start + 2, $asociados->lastPage());
+                                    $start = max(1, $asociados->currentPage() - 1);
+                                    $end = min($start + 2, $asociados->lastPage());
                                     @endphp
 
-                                    @for ($i = $start; $i <= $end; $i++)
-                                        <li class="page-item {{ $asociados->currentPage() == $i ? 'active' : '' }}">
-                                            <a class="page-link"
-                                                href="{{ $asociados->url($i) }}">{{ $i }}</a>
+                                    @for ($i = $start; $i <= $end; $i++) <li class="page-item {{ $asociados->currentPage() == $i ? 'active' : '' }}">
+                                        <a class="page-link" href="{{ $asociados->url($i) }}">{{ $i }}</a>
                                         </li>
-                                    @endfor
+                                        @endfor
 
-                                    <li class="page-item {{ $asociados->nextPageUrl() ? '' : 'disabled' }}">
-                                        <a class="page-link" href="{{ $asociados->nextPageUrl() }}">
-                                            <i class="now-ui-icons arrows-1_minimal-right"></i>
-                                        </a>
-                                    </li>
+                                        <li class="page-item {{ $asociados->nextPageUrl() ? '' : 'disabled' }}">
+                                            <a class="page-link" href="{{ $asociados->nextPageUrl() }}">
+                                                <i class="now-ui-icons arrows-1_minimal-right"></i>
+                                            </a>
+                                        </li>
                                 </ul>
                             </nav>
                         </div>
                     </div>
                     <div class="col-md-12">
-                        <button class="btn btn-primary">Agregar Cliente</button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalInsert">
+                            Agregar Cliente
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="ModalInsert" tabindex="-1" aria-labelledby="ModalInsertLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="ModalInsertLabel">Agregar Cliente</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="row">
+                                <div class="col-md-4 pr-1">
+                                    <div class="form-group">
+                                        <label>Cedula</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-4 px-1">
+                                    <div class="form-group">
+                                        <label>Apellidos</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-4 pl-1">
+                                    <div class="form-group">
+                                        <label>Nombre</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3 pr-1">
+                                    <div class="form-group">
+                                        <label>Distrito</label>
+                                        <select class="form-control" name="parentesco" aria-label="Default select example" id="selectDistrito"></select>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 px-1">
+                                    <div class="form-group">
+                                        <label>Parentesco</label>
+                                        <select class="form-control" name="parentesco" aria-label="Default select example" id="selectParentesco"></select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 pl-1">
+                                    <div class="form-group">
+                                        <label>Dirección</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3 pr-1">
+                                    <div class="form-group">
+                                        <label>Celular</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-3 px-1">
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 pl-1">
+                                    <div class="form-group">
+                                        <label>Dirección</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Observacion</label>
+                                        <input type="text" class="form-control" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09">
+                                    </div>
+                                </div>
+                            </div>                            
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-primary">Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         @include('footer')
 </body>
 
 </html>
-
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,6 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
@@ -28,20 +27,21 @@
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ms-2 text-sm text-gray-600">{{ __('Recuerdame') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('¿Olvidaste la contraseña?') }}
                     </a>
                 @endif
 
                 <x-button class="ms-4">
-                    {{ __('Log in') }}
+                    {{ __('Ingresar') }}
                 </x-button>
+
             </div>
         </form>
     </x-authentication-card>
