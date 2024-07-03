124fa0479d51103bac8d7d23fb2912f5f0e0fa09
vanesha <nva.gomo926@gmail.com>
2024-07-03 14:05:36 -0500

    Resposive index menu principal

--- resources/views/asociados/index.blade.php
+++ resources/views/asociados/index.blade.php
@@ -24,7 +24,7 @@
                             </button>
                         </div>
                         <a class="navbar-brand" href="#">Titular Cliente</a>
-                    </div>
+                    </div>                    
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
                         aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-bar navbar-kebab"></span>
--- resources/views/dashboardTemplate.blade.php
+++ resources/views/dashboardTemplate.blade.php
@@ -2,17 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8" />  
+  <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <x-component-header />
-</head> 
+</head>
 
 <body class="">
   <div class="wrapper ">
     @include('layouts.navbar')
     <div class="main-panel" id="main-panel">
-        
+
       <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
         <div class="container-fluid">
           <div class="navbar-wrapper">
@@ -23,40 +23,38 @@
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-
-            <a class="navbar-brand" href="#pablo">Hola, {{ Auth::user()->name }}</a>            
-            @yield('content')
-
+            <a class="navbar-brand" href="#">Hola, {{ Auth::user()->name }}</a>
           </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation">
-          <x-input-search></x-input-search>
-
-          <ul class="navbar-nav">
+          <div class="navbar-collapse justify-content-end collapse" id="navigation" style="">
+            <x-input-search></x-input-search>
+            <ul class="navbar-nav">
               <li class="nav-item">
-                  <a class="nav-link" href="/user/profile">
-                      <i class="now-ui-icons users_single-02"></i>
-                      <p>
-                          <span class="d-lg-none d-md-block">Account</span>
-                      </p>
-                  </a>
+                <a class="nav-link" href="/user/profile">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>
+                    <span class="d-lg-none d-md-block">Mi cuenta</span>
+                  </p>
+                </a>
               </li>
-          </ul>
+            </ul>            
           </div>
         </div>
+
+
       </nav>
       <!-- End Navbar -->
-      
+
       <div class="panel-header panel-header-lg vh-100">
         <!-- <canvas id="bigDashboardChart"></canvas> -->
-         
+
       </div>
-      
-      
+
+
       @include('layouts.footer')
 
     </div>
