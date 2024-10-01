72971a789f496eea8612f7559b7f38b78a3e9432
Vanessa <nva.gomo926@gmail.com>
2024-10-01 15:55:36 -0500

    organizar carpetas

---
+++ app/Http/Controllers/MoviContCincoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MoviContCinco;
+use Illuminate\Http\Request;
+
+class MoviContCincoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MoviContCinco $moviContCinco)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MoviContCinco $moviContCinco)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MoviContCinco $moviContCinco)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MoviContCinco $moviContCinco)
+    {
+        //
+    }
+}
