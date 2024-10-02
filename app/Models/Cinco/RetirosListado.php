5bb4f2a2aba20533c03f08ffb440968c4f48df70
Vanessa <nva.gomo926@gmail.com>
2024-10-02 14:05:03 -0500

    model controller retiros

---
+++ app/Models/Cinco/RetirosListado.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Cinco;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RetirosListado extends Model
+{
+    use HasFactory;
+}
