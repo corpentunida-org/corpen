4d2b1179f012627b17d1d3dfc2d2815071d8673a
Vanessa <nva.gomo926@gmail.com>
2025-01-17 15:53:55 -0500

    show terceros cinco

--- app/Http/Controllers/Cinco/TercerosController.php
+++ app/Http/Controllers/Cinco/TercerosController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers\Cinco;
 use App\Http\Controllers\Controller;
 use App\Models\Cinco\Terceros;
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuditoriaController;
 
 class TercerosController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+    private function auditoria($accion){
+        $auditoriaController = app(AuditoriaController::class);
+        $auditoriaController->create($accion, "CINCO");
+    }
+
     public function index()
     {
         $terceros = Terceros::paginate(6);
@@ -36,10 +43,10 @@ class TercerosController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public function show(Request $request)
     {
+        $id = $request->input('id');
         $tercero = Terceros::where('Cod_Ter', $id)->first();
-        dd($tercero);
         if (!$tercero) {
             return redirect()->route('cinco.tercero.index')->with('warning', 'No existe esa cédula en la lista de terceros cinco');
         }
@@ -57,9 +64,21 @@ class TercerosController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Terceros $terceros)
-    {
-        //
+    public function update(Request $request, $id)
+    {        
+        $ter = Terceros::where('Cod_Ter', $id)->first();
+        
+        $ter->update([
+            'Fec_Ing' => $request->input('Fec_Ing'),            
+            'Fec_Minis' => $request->input('Fec_Minis'),            
+            'Fec_Aport' => $request->input('Fec_Aport'),
+            'observacion'=> $request->input('observacion'),
+            'verificado'=> true,
+            'verificadousuario'=> auth()->user()->name
+        ]);
+        $accion = "Actualizar fechas terceros" . $id;
+        $this->auditoria($accion);
+        return redirect()->back()->with('success', 'Registro actualizado correctamente.');
     }
 
     /**
--- resources/views/layouts/actions/cinco.blade.php
+++ resources/views/layouts/actions/cinco.blade.php
@@ -1,6 +1,6 @@
 <li class="nxl-item nxl-hasmenu">
     <a href="javascript:void(0);" class="nxl-link">
-        <span class="nxl-micon"><i class="bi bi-graph-up-arrow"></i></span>
+        <span class="nxl-micon"><i class="bi bi-bar-chart-line-fill"></i></span>
         <span class="nxl-mtext">CINCO</span><span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
     </a>
     <ul class="nxl-submenu">
