4031d84a964693aa52ab8ebeec5db1447e3a69d0
vanesha <nva.gomo926@gmail.com>
2025-04-21 11:41:13 -0500

    index view poliza

--- resources/views/components/input-search-seguros.blade.php
+++ resources/views/components/input-search-seguros.blade.php
@@ -1,7 +1,23 @@
-<form action="{{ route('seguros.poliza.show', ['poliza' => 'ID']) }}" method="GET"
-    class="d-flex align-items-center gap-2 page-header-right-items-wrapper">
-    <label for="search-input" class="mb-0 me-2">Buscar:</label>
-    <input type="text" name="id" class="form-control form-control-sm" id="valueCedula" placeholder="cédula titular"
+{{-- <form action="{{ route('seguros.poliza.show', ['poliza' => 'ID']) }}" method="GET"
+    class="d-flex align-items-center gap-2 page-header-right-items-wrapper ml-2">
+    <label for="search-input" class="mb-0">Buscar por:</label>
+    <input type="text" name="id" class="form-control" id="valueCedula" placeholder="cédula titular"
         aria-controls="customerList">
     <button type="submit" class="btn btn-primary"><i class="bi bi-search"></i></button>
+</form> --}}
+
+<form action="{{ route('seguros.poliza.show', ['poliza' => 'ID']) }}" method="GET"
+    class="row gx-2 align-items-center ml-2">
+    <div class="col-auto">
+        <label for="search-input" class="col-form-label mb-0">Buscar por:</label>
+    </div>
+    <div class="col">
+        <input type="text" name="id" class="form-control" id="valueCedula" placeholder="cédula titular"
+            aria-controls="customerList">
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary">
+            <i class="bi bi-search"></i>
+        </button>
+    </div>
 </form>
--- resources/views/seguros/polizas/search.blade.php
+++ resources/views/seguros/polizas/search.blade.php
@@ -1,68 +1,54 @@
 <x-base-layout>
     @section('titlepage', 'Filtro por nombre')
     <div class="col-12">
-        <div class="card stretch stretch-full">
-            <div class="card-body">
+        <div class="card">
+            <div class="card-body py-1 m-0">
                 <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <div class="page-header-right ms-auto">
-                            <div class="page-header-right-items">
-                                <div class="d-flex align-items-center gap-2 page-header-right-items-wrapper">
-                                    <a href="javascript:void(0);" class="btn btn-icon btn-light-brand"
-                                        data-bs-toggle="collapse" data-bs-target="#collapseOne">
-                                        <i class="feather-bar-chart"></i>
-                                    </a>
-                                    <a href="" class="btn btn-primary">
-                                        <i class="feather-plus me-2"></i>
-                                        <span>Crear Poliza</span>
-                                    </a>
+                    <div class="col-sm-12 col-md-7">
+                        <div class="content-area ps ps--active-y" style="height: 100px;">
+                            <div class="content-area-header bg-white sticky-top m-0 p-0">
+                                <div class="page-header-right ms-auto justify-content-end m-0 pt-4">
+                                    <div class="hstack gap-2 m-0">
+                                        <p class="fs-12 fw-bold text-dark pt-3">Buscar por nombre: </p>
+                                        <div class="hstack">
+                                            <a href="" class="search-form-open-toggle">
+                                                <div class="avatar-text avatar-md" data-bs-toggle="tooltip"
+                                                    data-bs-trigger="hover" title=""
+                                                    data-bs-original-title="Search">
+                                                    <i class="feather-search"></i>
+                                                </div>
+                                            </a>
+                                            <form action="{{ route('poliza.search', ['name' => 'ID']) }}" method="GET"
+                                                class="search-form" style="display: none">
+                                                <div class="search-form-inner pt-4">
+                                                    <a href="" class="search-form-close-toggle">
+                                                        <div class="avatar-text avatar-md" data-bs-toggle="tooltip"
+                                                            data-bs-trigger="hover" title=""
+                                                            data-bs-original-title="Back">
+                                                            <i class="feather-arrow-left"></i>
+                                                        </div>
+                                                    </a>
+                                                    <input type="search" name="id"
+                                                        class="px-0 border-0 w-100"
+                                                        placeholder="Buscar por nombre..." autocomplete="off">
+                                                    <button type="submit" class="btn btn-primary">Buscar</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="d-md-none d-flex align-items-center">
-                                <a href="javascript:void(0)" class="page-header-right-open-toggle">
-                                    <i class="feather-align-right fs-20"></i>
-                                </a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-12 col-md-6 d-flex justify-content-end align-items-center">
+                    <div class="col-sm-12 col-md-5 d-flex justify-content-end align-items-center">
                         <x-input-search-seguros></x-input-search-seguros>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="content-area ps ps--active-y" style="height: 100px;">
-        <div class="content-area-header bg-white sticky-top">
-            <div class="page-header-right ms-auto">
-                <div class="hstack gap-2">
-                    <p class="fs-12 text-muted pt-3">Buscar por nombre: </p>
-                    <div class="hstack">
-                        <a href="javascript:void(0)" class="search-form-open-toggle">
-                            <div class="avatar-text avatar-md" data-bs-toggle="tooltip" data-bs-trigger="hover"
-                                title="" data-bs-original-title="Search">
-                                <i class="feather-search"></i>
-                            </div>
-                        </a>
-                        <form action="{{ route('poliza.search', ['name' => 'ID']) }}" method="GET" class="search-form"
-                            style="display: none">
-                            <div class="search-form-inner">
-                                <a href="javascript:void(0)" class="search-form-close-toggle">
-                                    <div class="avatar-text avatar-md" data-bs-toggle="tooltip" data-bs-trigger="hover"
-                                        title="" data-bs-original-title="Back">
-                                        <i class="feather-arrow-left"></i>
-                                    </div>
-                                </a>
-                                <input type="search" name="id" class="py-3 px-0 border-0 w-100"placeholder="Buscar por nombre..." autocomplete="off">
-                                <button type="submit" class="btn btn-primary">Buscar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
+    
     <div class="col-xxl-12">
         <div class="card stratch">
             <div class="card-header">
