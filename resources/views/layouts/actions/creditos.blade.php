d38840c7cfeef9a11787fef39d978bae62b2c57b
BrayamCode <stived1998@gmail.com>
2025-08-19 14:43:33 -0500

    GestionDoc

--- resources/views/layouts/actions/creditos.blade.php
+++ resources/views/layouts/actions/creditos.blade.php
@@ -1,4 +1,4 @@
-<li class="nxl-item nxl-hasmenu {{ request()->routeIs('creditos.estado1.*', 'creditos.estado2.*', 'creditos.estado3.*', 'creditos.estado4.*', 'creditos.estado5.*', 'creditos.estado6.*', 'creditos.estado7.*') ? 'active' : '' }}">
+<li class="nxl-item nxl-hasmenu {{ request()->routeIs('creditos.documentacion.*', 'creditos.analisis.*', 'creditos.desembolso.*', 'creditos.tesoreria.*', 'creditos.cartera.*', 'creditos.recaudo.*') ? 'active' : '' }}">
     <a href="javascript:void(0);" class="nxl-link">
         <span class="nxl-micon"><i class="bi bi-credit-card-2-front"></i></span>
         <span class="nxl-mtext">Créditos</span>
@@ -6,19 +6,23 @@
     </a>
 
     <ul class="nxl-submenu">
-        {{-- Etapa 1 --}}
-        <li class="nxl-item {{ request()->routeIs('estado1.index') ? 'active' : '' }}">
-            <a href="javascript:void(0);" class="nxl-link">
-                Maestras
-                <span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
-            </a>
-            <ul class="nxl-submenu">
-                <li class="nxl-item">
-                    <a class="nxl-link" href="{{ route('estado1.index') }}" style="text-decoration: none;">
-                        Lista de Fábricas
-                    </a>
-                </li>
-            </ul>
+        <li class="nxl-item">
+            <a class="nxl-link" href="#">Documentación</a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="#">Análisis</a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="#">Desembolso</a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="#">Tesorería</a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="#">Cartera</a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="#">Recaudo</a>
         </li>
     </ul>
 </li>
