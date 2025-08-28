decabc0cd50d4c5544393ea642548823ea8d1909
BrayamCode <stived1998@gmail.com>
2025-08-28 11:31:16 -0500

    gestion

--- resources/views/layouts/actions/gestion.blade.php
+++ resources/views/layouts/actions/gestion.blade.php
@@ -6,52 +6,46 @@
     </a>
 
     <ul class="nxl-submenu">
-        {{-- Submenú para Empleados y Documentos --}}
         <li class="nxl-item nxl-hasmenu {{ request()->routeIs('archivo.empleado.*', 'archivo.cargo.*', 'archivo.area.*', 'archivo.categoria.*', 'archivo.gdotipodocumento.*', 'archivo.gdodocsempleados.*') ? 'active' : '' }}">
             <a href="javascript:void(0);" class="nxl-link">
-                <span class="nxl-micon"><i class="bi bi-people-fill"></i></span>
+                <span class="nxl-micon"><i class="bi bi-people"></i></span>
                 Empleados & Documentos
                 <span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
             </a>
             <ul class="nxl-submenu">
                 <li class="nxl-item {{ request()->routeIs('archivo.empleado.index') ? 'active' : '' }}">
-                    <a class="nxl-link" href="{{ route('archivo.empleado.index') }}" style="text-decoration: none;">
-                        <span class="nxl-micon"><i class="bi bi-person-fill"></i></span>
+                    <a class="nxl-link" href="{{ route('archivo.empleado.index') }}">
+                        <span class="nxl-micon"><i class="bi bi-person"></i></span>
                         Empleados
                     </a>
                 </li>
-                
                 <li class="nxl-item {{ request()->routeIs('archivo.cargo.index') ? 'active' : '' }}">
-                    <a class="nxl-link" href="{{ route('archivo.cargo.index') }}" style="text-decoration: none;">
-                        <span class="nxl-micon"><i class="bi bi-briefcase-fill"></i></span>
+                    <a class="nxl-link" href="{{ route('archivo.cargo.index') }}">
+                        <span class="nxl-micon"><i class="bi bi-briefcase"></i></span>
                         Cargos
                     </a>
                 </li>
-
                 <li class="nxl-item {{ request()->routeIs('archivo.area.index') ? 'active' : '' }}">
-                    <a class="nxl-link" href="{{ route('archivo.area.index') }}" style="text-decoration: none;">
-                        <span class="nxl-micon"><i class="bi bi-grid-fill"></i></span>
+                    <a class="nxl-link" href="{{ route('archivo.area.index') }}">
+                        <span class="nxl-micon"><i class="bi bi-grid"></i></span>
                         Áreas
                     </a>
                 </li>
-
                 <li class="nxl-item {{ request()->routeIs('archivo.categorias.index') ? 'active' : '' }}">
-                    <a class="nxl-link" href="{{ route('archivo.categorias.index') }}" style="text-decoration: none;">
-                        <span class="nxl-micon"><i class="bi bi-tags-fill"></i></span>
+                    <a class="nxl-link" href="{{ route('archivo.categorias.index') }}">
+                        <span class="nxl-micon"><i class="bi bi-tags"></i></span>
                         Categorías de Documentos
                     </a>
                 </li>
-
                 <li class="nxl-item {{ request()->routeIs('archivo.gdotipodocumento.index') ? 'active' : '' }}">
-                    <a class="nxl-link" href="{{ route('archivo.gdotipodocumento.index') }}" style="text-decoration: none;">
-                        <span class="nxl-micon"><i class="bi bi-file-earmark-text-fill"></i></span>
+                    <a class="nxl-link" href="{{ route('archivo.gdotipodocumento.index') }}">
+                        <span class="nxl-micon"><i class="bi bi-file-earmark-text"></i></span>
                         Tipo de Documento
                     </a>
                 </li>
-
                 <li class="nxl-item {{ request()->routeIs('archivo.gdodocsempleados.index') ? 'active' : '' }}">
-                    <a class="nxl-link" href="{{ route('archivo.gdodocsempleados.index') }}" style="text-decoration: none;">
-                        <span class="nxl-micon"><i class="bi bi-file-check-fill"></i></span>
+                    <a class="nxl-link" href="{{ route('archivo.gdodocsempleados.index') }}">
+                        <span class="nxl-micon"><i class="bi bi-file-check"></i></span>
                         Documentos
                     </a>
                 </li>
