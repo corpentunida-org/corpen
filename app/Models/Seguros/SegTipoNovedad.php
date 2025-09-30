7ff33b9aee8e743881315ab7213ba36ee6afd63e
Vanessa <nva.gomo926@gmail.com>
2025-09-30 21:30:15 +0000

    novedades update

--- app/Models/Seguros/SegTipoNovedad.php
+++ app/Models/Seguros/SegTipoNovedad.php
@@ -5,7 +5,12 @@ namespace App\Models\Seguros;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SegTipoPoliza extends Model
+class SegTipoNovedad extends Model
 {
     use HasFactory;
+    protected $table = 'Seg_TipoNovedad';
+    protected $fillable = [
+        'id',
+        'nombre',  
+    ];
 }
