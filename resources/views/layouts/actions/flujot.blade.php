c62bc453d28ebc383f5b248a4c92de8d37561181
BrayamCode <stived1998@gmail.com>
2025-11-05 15:53:20 -0500

    Detalless

--- resources/views/layouts/actions/flujot.blade.php
+++ resources/views/layouts/actions/flujot.blade.php
@@ -1,14 +1,46 @@
+<!-- ===== Menú: Flujo de Trabajo ===== -->
 <li class="nxl-item nxl-hasmenu">
     <a class="nxl-link">
         <span class="nxl-micon"><i class="bi bi-kanban"></i></span>
         <span class="nxl-mtext">Flujo de trabajo</span>
         <span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
     </a>
+
     <ul class="nxl-submenu">
-        {{-- Enlace a Flujo de Trabajo --}}
+        <!-- Submenú: Flujo de Trabajo -->
         <li class="nxl-item">
             <a class="nxl-link" href="{{ route('flujo.tablero') }}">
-                Flujo de Trabajo
+                <i class="bi bi-house-door me-2"></i> Inicio
+            </a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="{{ route('flujo.tablero') }}">
+                <i class="bi bi-plus-circle me-2"></i> Crear Tarea
+            </a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="{{ route('flujo.tablero') }}">
+                <i class="bi bi-list-check me-2"></i> Mis Tareas
+            </a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="{{ route('flujo.tablero') }}">
+                <i class="bi bi-bell me-2"></i> Notificaciones
+            </a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="{{ route('flujo.tablero') }}">
+                <i class="bi bi-plus-circle me-2"></i> Crear Espacio
+            </a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="{{ route('flujo.tablero') }}">
+                <i class="bi bi-folder2-open me-2"></i> Mis Espacios
+            </a>
+        </li>
+        <li class="nxl-item">
+            <a class="nxl-link" href="{{ route('flujo.tablero') }}">
+                <i class="bi bi-graph-up me-2"></i> Informes
             </a>
         </li>
     </ul>
