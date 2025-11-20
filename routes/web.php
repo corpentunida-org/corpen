6bac01ec6c3449568ee9f5fa39fd550ac2df192e
BrayamCode <stived1998@gmail.com>
2025-11-20 10:44:19 -0500

    INTERACCIONES 004

--- routes/web.php
+++ routes/web.php
@@ -324,7 +324,8 @@ Route::prefix('interactions')
     
     // 📌 AJAX: Buscar clientes para Select2
     Route::get('/search-clients', [InteractionController::class, 'searchClients'])->name('search-clients');
-
+    // 🆕 NUEVA RUTA: Obtener el distrito de un cliente
+    Route::get('/clientes/{client_id}/distrito', [InteractionController::class, 'getClientDistrict'])->name('clientes.distrito');
     // --- 📡 GRUPO DE RUTAS PARA CANALES DE INTERACCIÓN ---
     Route::prefix('channels')->name('channels.')->group(function () {
         Route::get('/', [IntChannelController::class, 'index'])->name('index');
