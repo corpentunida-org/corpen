5b5856f6df8a26de42c55e379e5ecd240c6e1046
Vanessa <nva.gomo926@gmail.com>
2025-12-12 15:30:46 +0000

    upload valor a pagar

--- app/Http/Controllers/Seguros/SegPolizaController.php
+++ app/Http/Controllers/Seguros/SegPolizaController.php
@@ -324,7 +324,7 @@ class SegPolizaController extends Controller
         foreach ($rows as $index => $row) {
             $modeloData = SegPoliza::where('seg_asegurado_id', $row['cod_ter'])->first();
             if ($modeloData) {
-                $modeloData::update([
+                $modeloData->update([
                     'primapagar' => $row['deb_mov'],
                     'valorpagaraseguradora' => $row['pagar_aco'],
                 ]);
