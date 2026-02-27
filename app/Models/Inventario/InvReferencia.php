786b19db45a30e7aaf3fcf3fb22bc37d6db65829
Brayam <stived1998@gmail.com>
2026-02-27 21:32:03 +0000

    sistema de inventario 3

--- app/Models/Inventario/InvReferencia.php
+++ app/Models/Inventario/InvReferencia.php
@@ -6,39 +6,54 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Inventario\InvDetalleCompra;
 use App\Models\Inventario\InvActivo;
+use App\Models\Inventario\InvSubgrupo;
+use App\Models\Inventario\InvBodega; 
 
 class InvReferencia extends Model
 {
     use HasFactory;
 
-    // Indicamos explícitamente la tabla
     protected $table = 'inv_referencias';
 
-    // Campos permitidos para asignación masiva
     protected $fillable = [
-        'nombre',
+        'referencia',
         'detalle',
+        'id_InvSubGrupos',
+        'id_InvBodegas',
     ];
 
     /**
+     * Relación con el Subgrupo (Padre)
+     * Una Referencia pertenece a un Subgrupo.
+     */
+    public function subgrupo()
+    {
+        return $this->belongsTo(InvSubgrupo::class, 'id_InvSubGrupos');
+    }
+
+    /**
+     * Relación con la Bodega (Padre)
+     * Una Referencia pertenece a una Bodega.
+     */
+    public function bodega()
+    {
+        // 'id_InvBodegas' es la llave foránea en esta tabla (inv_referencias)
+        return $this->belongsTo(InvBodega::class, 'id_InvBodegas');
+    }
+
+    /**
      * Relación con los Detalles de Compra
-     * Una Referencia (producto/ítem) puede estar presente en muchos detalles de compra.
      */
     public function detallesCompra()
     {
-        // El segundo parámetro es la llave foránea en la tabla 'inv_detalle_compras'
-        // El tercer parámetro es la llave local en esta tabla 'inv_referencias'
         return $this->hasMany(InvDetalleCompra::class, 'invReferencias_id', 'id');
     }
 
     /**
      * Relación con los Activos
-     * Una Referencia puede tener asociados muchos activos físicos.
      */
     public function activos()
     {
-        // El segundo parámetro es la llave foránea en la tabla 'inv_activos'
-        // El tercer parámetro es la llave local en esta tabla 'inv_referencias'
         return $this->hasMany(InvActivo::class, 'invReferencias_id', 'id');
     }
 }
