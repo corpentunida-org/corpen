24c70a744ff0666ef91476bb0a41351110de1ee6
Brayam <stived1998@gmail.com>
2026-03-04 21:46:08 +0000

    sistema de inventario 6

--- app/Models/Inventario/InvEstado.php
+++ app/Models/Inventario/InvEstado.php
@@ -5,16 +5,34 @@ namespace App\Models\Inventario;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Inventario\InvActivo;
+use App\Models\Inventario\InvBodega;
 
 class InvEstado extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $table = 'inv_estados'; // [cite: 180]
-    protected $fillable = ['nombre'];
+    protected $table = 'inv_estados';
 
+    protected $fillable = [
+        'nombre',
+        'detalle',
+        'id_bodega'
+    ];
+
+    /**
+     * Relación: Un Estado tiene muchos Activos
+     */
     public function activos()
     {
         return $this->hasMany(InvActivo::class, 'id_Estado');
+    }
+
+    /**
+     * Relación: Un Estado pertenece a una Bodega
+     */
+    public function bodega()
+    {
+        return $this->belongsTo(InvBodega::class, 'id_bodega');
     }
 }
